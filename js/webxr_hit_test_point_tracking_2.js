af160abb5bddbbdaf7b67917a60b568a92e9cb91
RaghavS-TietoEvry <raghavendra.sinha@tietoevry.com>
2023-11-21 20:00:30 +0530

    Updated webXR codes

--- js/webxr_hit_test_point_tracking_2.js
+++ js/webxr_hit_test_point_tracking_2.js
@@ -1,5 +1,5 @@
 import * as THREE from "../node_modules/three/src/Three.js";
-import { GLTFLoader } from "https://cdn.skypack.dev/three@0.134.0/examples/jsm/loaders/GLTFLoader.js";
+// import { GLTFLoader } from "https://cdn.skypack.dev/three@0.134.0/examples/jsm/loaders/GLTFLoader.js";
 
 // import * as THREE from "three";
 // import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
